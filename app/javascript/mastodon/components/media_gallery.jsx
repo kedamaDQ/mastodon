c33bc2a242765f53e2c412931024da5b06ab6124
kedamaDQ <kedama@foresdon.jp>
2025-07-13 16:15:01 +0900

    Revert "Hide badges in media gallery when media are hidden (#32224)"
    This reverts commit 55b5364534a97c66b8612e6611ef0bba19b349d2.

--- app/javascript/mastodon/components/media_gallery.jsx
+++ app/javascript/mastodon/components/media_gallery.jsx
@@ -196,7 +196,7 @@ class Item extends PureComponent {
 
         {visible && thumbnail}
 
-        {visible && badges && (
+        {badges && (
           <div className='media-gallery__item__badges'>
             {badges}
           </div>
