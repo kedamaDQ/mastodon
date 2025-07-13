8a7a7d58ffc17edb8b1a6a0096d1b602425cc591
kedamaDQ <kedama@foresdon.jp>
2025-07-13 16:17:16 +0900

    Revert "Support /.well-known/host-meta.json (#32206)"
    This reverts commit cbf134937054dadb037350cb390b9612450c8062.

--- spec/routing/well_known_routes_spec.rb
+++ spec/routing/well_known_routes_spec.rb
@@ -4,14 +4,9 @@ require 'rails_helper'
 
 RSpec.describe 'Well Known routes' do
   describe 'the host-meta route' do
-    it 'routes to correct place' do
+    it 'routes to correct place with xml format' do
       expect(get('/.well-known/host-meta'))
-        .to route_to('well_known/host_meta#show')
-    end
-
-    it 'routes to correct place with json format' do
-      expect(get('/.well-known/host-meta.json'))
-        .to route_to('well_known/host_meta#show', format: 'json')
+        .to route_to('well_known/host_meta#show', format: 'xml')
     end
   end
 
