6794f3b1abcc9481ffcf9c6c74e6f4b93f27ca42
kedamaDQ <kedama@foresdon.jp>
2026-08-18 14:20:12 +0900

    Show local timeline using advanced layout by default
    default settings for new accounts

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -21,7 +21,7 @@ class UserSettings
   setting_inverse_alias :indexable, :noindex
 
   namespace :web do
-    setting :advanced_layout, default: false
+    setting :advanced_layout, default: true
     setting :trends, default: true
     setting :use_blurhash, default: true
     setting :use_pending_items, default: false
